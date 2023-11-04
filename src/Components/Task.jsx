69294ee8594a68afaf161c3cc98c6f4fc5e252d4
arkzse <88483186+arkzuse@users.noreply.github.com>
2023-11-04 16:07:56 +0530

    add: Task component

---
+++ src/Components/Task.jsx
@@ -0,0 +1,58 @@
+import {useTasksDispatch} from "../utils/TaskContext.jsx";
+import {useState} from "react";
+import {useUser} from "../utils/UserContext.jsx";
+
+export default function Task({task}) {
+    const user = useUser().cur
+    const dispatch = useTasksDispatch()
+
+    const [isEditing, setIsEditing] = useState()
+    const [text, setText] = useState(task.text)
+    let taskContent;
+
+    function handleDone() {
+        dispatch({type: 'toggleDone', payload: {id: task.id, username: user}})
+    }
+
+    function handleSave() {
+        dispatch({type: 'rename', payload: {id: task.id, text: text, username: user}})
+        setIsEditing(false)
+    }
+
+    function handleDelete() {
+        dispatch({type: 'delete', payload: {id: task.id, username: user}})
+    }
+
+    if (isEditing) {
+        taskContent = (
+            <>
+                <input
+                    value={text}
+                    onChange={e => setText(e.target.value)}
+                />
+                <button onClick={() => handleSave()}>Save</button>
+            </>
+        )
+    } else {
+        taskContent = (
+            <>
+                {task.text}
+                <button onClick={() => setIsEditing(true)}>Edit</button>
+            </>
+        )
+    }
+
+    return (
+        <div>
+            <label>
+                <input
+                    type='checkbox'
+                    checked={task.done}
+                    onChange={() => handleDone()}
+                />
+                {taskContent}
+                <button onClick={() => handleDelete()}>Delete</button>
+            </label>
+        </div>
+    )
+}
